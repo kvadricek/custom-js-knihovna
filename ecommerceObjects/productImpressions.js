028ebb6b0aaa6b9818216be0e19068261943e763
Karol <kaja.markytanova@gmail.com>
2019-09-10 12:41:31 +0200

    Update productImpressions.js

--- ecommerceObjects/productImpressions.js
+++ ecommerceObjects/productImpressions.js
@@ -1,5 +1,5 @@
 function(){
-var ecm = {{dl.ecommerce}},
+var ecm = {{dl.ecommerce}},  // 
     formateProductData = {{fn.formateProductData ---product}},
     ld,
     list,
@@ -8,14 +8,14 @@ var ecm = {{dl.ecommerce}},
     if (ecm && ecm.impressions) {
       ld = ecm.impressions.listDetail;
         if (ecm.impressions.listDetail) {
-            list = ld.listLocation + '-' + ld.listType + '-' + ld.menuLevel + '-' + ld.filtr;
+            list = ld.listLocation + '-' + ld.listType + '-' + ld.menuLevel + '-' + ld.filtr; // here you go into the previously defined listDetail object and you put together one list variable from all the key:values the object contains 
         }
 		if  (ecm.impressions.products) {
           prods = ecm.impressions.products,
-            imps = [];
-          for (var i = 0; i < prods.length; i++) {
-            prods[i].list=list;
-            imps.push(formateProductData(prods[i]));
+            imps = [];                               //defining the variable as an empty array allows you to populate it later 
+          for (var i = 0; i < prods.length; i++) {   // goes through each individual products as long as there are any 
+            prods[i].list=list;                     // since products is a {object}, you cannot use the push method so you use dot notation instead (basically you take each product and you assing it a new key which has the value of a previously defined list variable  
+            imps.push(formateProductData(prods[i])); // now you take the products object which now contains the list key:value, you formate it with the formateProduct function and you use push method to add it to the imps array
           }
         }  
     }
